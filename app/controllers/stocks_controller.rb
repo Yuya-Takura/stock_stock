923d387952f45b93dc5cf5eab4d97763c04d9594
tequila <yuya.arukat@gmail.com>
2019-01-23 23:46:03 +0900

    feat: Added stocks show page

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,4 +1,8 @@
 class StocksController < ApplicationController
+  def show
+    @stock = Stock.find(params[:id])
+  end
+
   def search
     keyword = NKF.nkf('-Z1, -w', params[:keyword])
     @stocks = Stock.ransack(code_or_name_cont: keyword).result
