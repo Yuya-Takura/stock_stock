3b37c7d98b15a86e04e759c17fe0f8a60d460524
tequila <yuya.arukat@gmail.com>
2019-02-03 01:43:40 +0900

    chore: Added user agent in scrape proc

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,7 @@
 class Stock < ApplicationRecord
   def self.scrape_url(code)
+    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.81 Safari/537.36"
+
     stock = find_by(code: code)
     search_url = "https://www.google.co.jp/search?hl=jp&gl=JP&"
     query = URI.encode_www_form(q: "#{stock.name}")
@@ -7,14 +9,14 @@ class Stock < ApplicationRecord
 
     charset = nil
 
-    html = open(search_url) do |f|
+    html = open(search_url, 'User-Agent' => user_agent) do |f|
       charset = f.charset
       f.read
     end
 
     doc = Nokogiri::HTML.parse(html, nil, charset)
 
-    link = doc.xpath('//div[@class="hJND5c"]')
-    link[0].child.text
+    link = doc.xpath('//div[@class="r"]/a')
+    link[0].attribute('href').text
   end
 end
