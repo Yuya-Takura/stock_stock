d33ab93a5a0b685fb95ecf1d8ef12609de8d6ca1
tequila <yuya.arukat@gmail.com>
2019-02-08 07:57:56 +0900

    validate: Added validation for settlement model

--- app/models/settlement.rb
+++ app/models/settlement.rb
@@ -1,2 +1,13 @@
 class Settlement < ApplicationRecord
+  QUARTER_COUNT = {
+      first: 1,
+      second: 2,
+      third: 3,
+      fourth: 4,
+  }.freeze
+
+  validates :quarter, numericality: {
+      :greater_than_or_equal_to => QUARTER_COUNT[:first],
+      :less_than_or_equal_to => QUARTER_COUNT[:fourth],
+  }
 end
