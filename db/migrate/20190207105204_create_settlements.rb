9b245ffadd50bee7f98c776cb9585f90181cfb10
tequila <yuya.arukat@gmail.com>
2019-02-11 21:22:37 +0900

    fix: Modified migration file

--- db/migrate/20190207105204_create_settlements.rb
+++ db/migrate/20190207105204_create_settlements.rb
@@ -1,11 +1,11 @@
 class CreateSettlements < ActiveRecord::Migration[5.2]
   def change
     create_table :settlements do |t|
-      t.date :date, null: false
       t.references :stock, foreign_key: true, index: false, null: false
       t.string :stock_code, null: false
       t.integer :year, null: false
       t.integer :quarter, null: false
+      t.date :date, null: false
       t.float :eps, null: false
       t.float :expected_eps, null: false
 
