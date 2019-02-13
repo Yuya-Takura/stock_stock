66ae10a042837b9dddb46d413a0c8f1c69d1bf34
tequila <yuya.arukat@gmail.com>
2019-02-13 19:57:34 +0900

    feat: Added validation and RSpec file for settlement model

--- app/models/settlement.rb
+++ app/models/settlement.rb
@@ -6,8 +6,13 @@ class Settlement < ApplicationRecord
       fourth: 4,
   }.freeze
 
-  validates :quarter, numericality: {
-      :greater_than_or_equal_to => QUARTER_COUNT[:first],
-      :less_than_or_equal_to => QUARTER_COUNT[:fourth],
+  validates :stock_id, presence: true
+  validates :stock_code, presence: true
+  validates :year, presence: true
+  validates :quarter, presence: true, numericality: {
+    :greater_than_or_equal_to => QUARTER_COUNT[:first],
+    :less_than_or_equal_to => QUARTER_COUNT[:fourth],
   }
+  validates :date, presence: true
+  validates :eps, presence: true
 end
